a6a86acee1aaec204baad88b6105eb26de259d0f
aniharu <haruya04183083@gmail.com>
2017-09-04 20:15:47 +0900

    update

--- analysis/basic_statistic_analysis.py
+++ analysis/basic_statistic_analysis.py
@@ -3,6 +3,11 @@
 
 import numpy as np
 import pandas as pd
+from numba import jit
+import time
+import seaborn as sns
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
 
 #データ読み込み
 def read_data():
@@ -13,8 +18,25 @@ def read_data():
     return trainA,trainB,trainC,trainD
 
 #基本統計量算出
+#@jit
 def statistic_analysis(data):
+    st=time.time()
+    print(data.columns)
     print('number of uniqe ids: '+str(data.user_id.value_counts().count()))
+    print('\nコンバージョン:3, クリック:2, 閲覧:1, カート:0')
+    print(data['event_type'].value_counts())
+    print('\n')
+    print(data['ad'].value_counts())
+
+    #ユニークIDを取得
+    unique=data.user_id.unique()
+    num = []
+    for i in unique:
+        tmp=data[data['user_id'].isin([i])]
+        num.append(len(tmp))
+    print('Time : '+str(time.time() - st))
+    sns.distplot(num)
+    plt.show()
 
 if __name__=='__main__':
     a,b,c,d=read_data()
